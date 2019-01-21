49e4e2a8fdbd410d883045e4f945f52fdc635eee
FloWebDev <mathevon.florian@gmail.com>
2019-01-21 15:05:55 +0100

    second commit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="home_page")
+     * @Route("/", name="home_page", methods={"GET"})
      */
     public function home()
     {
@@ -25,7 +25,52 @@ class DefaultController extends AbstractController
         dd($activePresentations);
 
         return $this->render('default/home.html.twig', [
-            // 'controller_name' => 'DefaultController',
+            'allPresentations' => $allPresentations,
+            'activePresentations' => $activePresentations
+        ]);
+    }
+
+    /**
+     * @Route("/mentions-legales", name="mention", methods={"GET"})
+     */
+    public function mention()
+    {
+        
+        return $this->render('default/mention.html.twig', [
+            
+        ]);
+    }
+
+    /**
+     * @Route("/faq", name="faq", methods={"GET"})
+     */
+    public function faq()
+    {
+        
+        return $this->render('default/faq.html.twig', [
+            
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact", methods={"GET", "POST"})
+     */
+    public function contact()
+    {
+        
+        return $this->render('default/contact.html.twig', [
+            
+        ]);
+    }
+
+    /**
+     * @Route("/search", name="search", methods={"GET", "POST"})
+     */
+    public function search()
+    {
+        
+        return $this->render('default/search.html.twig', [
+            
         ]);
     }
 }
