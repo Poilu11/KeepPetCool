7bd64e791e52383be81d58329a34cb1e9d934ab5
SimStuch <simon.sauzede@gmail.com>
2019-01-22 10:22:29 +0100

    inté faq sans responsive

--- src/Migrations/Version20190122084512.php
+++ src/Migrations/Version20190122084512.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190121153651 extends AbstractMigration
+final class Version20190122084512 extends AbstractMigration
 {
     public function getDescription() : string
     {
