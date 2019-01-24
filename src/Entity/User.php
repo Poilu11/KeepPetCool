ba220f277a0d7429b6cb60383254ab9a9bd2dbc1
FloWebDev <mathevon.florian@gmail.com>
2019-01-24 14:29:10 +0100

    gestion avatar pour edit user et bouton suppression avatar

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -159,7 +159,7 @@ class User implements UserInterface, \Serializable
         return null;
     }
 
-    public function getPassword(): ?string
+    public function getPassword()
     {
         return $this->password;
     }
@@ -387,7 +387,7 @@ class User implements UserInterface, \Serializable
         return $this;
     }
 
-    public function setPassword(string $password): self
+    public function setPassword($password): self
     {
         $this->password = $password;
 
