fe534dd9b49744bec971e4b1277100d8b82a124d
FloWebDev <mathevon.florian@gmail.com>
2019-02-04 15:12:54 +0100

    test mail mohamed

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -70,7 +70,7 @@ class MessageController extends AbstractController
 
             // DEBUT SWIFT MAILER
                 $message = (new \Swift_Message('KeepPetCool - Nouveau message !'))
-                ->setFrom('keeppetcool@gmail.com')
+                ->setFrom('test74000@elgnani.fr')
                 ->setTo($userTo->getEmail())
                 ->setBody(
                 $this->renderView(
