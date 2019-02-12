e8295a9d7088f8294ddf2659b579615aba9a7301
SimStuch <simon.sauzede@gmail.com>
2019-02-12 13:57:20 +0100

    merge ok

--- chat/runServer.php
+++ chat/runServer.php
@@ -12,6 +12,13 @@
 *   Il faut ensuite repérer le numéro du processus puis faire la commande : kill numéro
 */
 
+/*  Pour déployer le chat en sécure.
+*   Dans runServer.php : insérer l'ip du serveur de déploiement en tant que 3eme paramètre de la fonction factory. Il doit etre passé sous forme de chaine de caractère.
+*   Dans le .env : mettre le nom de domaine dans wp_server_ip. mettre le numéro de port dans wp_server_port, mettre wp_server_secure = true si connection https.
+*   Dans le vhost : rediriger toutes les requête pour des websocket vers le serveur de websocket (inutile en http, essentiel en https).
+* RewriteCond %{HTTP:Upgrade} =websocket [NC]
+RewriteRule  ""    ws://chat.keeppetcool.com:8080 [P,L]
+*/
 
 use Chat\ChatApp;
 use Ratchet\Http\HttpServer;
@@ -23,6 +30,6 @@ use Ratchet\WebSocket\WsServer;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$server = IoSecureServer::factory(new HttpServer(new WsServer(new ChatApp())) ,8080, '92.243.16.8');
+$server = IoSecureServer::factory(new HttpServer(new WsServer(new ChatApp())) ,8080, '127.0.0.1');
 
 $server->run();
